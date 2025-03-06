f16b0530b27e7be09583b95a784149bd3d38918b
dungvv1.ho <dungvv1.ho@vietcombank.com.vn>
2025-03-06 08:44:26 +0700

    c

--- app.py
+++ app.py
@@ -301,24 +301,7 @@ if st.button("Send"):
                     AUTO = Cho vay mua ô tô/ xe máy để tiêu dùng => ModelSegmentation
                     ô tô = Cho vay mua ô tô/ xe máy để tiêu dùng => ModelSegmentation
                     oto = Cho vay mua ô tô/ xe máy để tiêu dùng => ModelSegmentation
-                    FX = FX => ModelSegmentation
-                    ngoại tệ = FX => ModelSegmentation
-                    ngoại hối = FX => ModelSegmentation
-                    IRS = IRS => ModelSegmentation
-                    interest rate swap = IRS => ModelSegmentation
-                    hợp đồng hoán đổi lãi suất = IRS => ModelSegmentation
-                    hoán đổi lãi suất. IRS thuộc phái sinh lãi suất (PSLS) = IRS => ModelSegmentation
-                    CCS = CCS => ModelSegmentation
-                    cross currency swap = CCS => ModelSegmentation
-                    hợp đồng hoán đổi ngoại tệ = CCS => ModelSegmentation
-                    hoán đổi ngoại tệ. CCS thuộc phái sinh lãi suất (PSLS) = CCS => ModelSegmentation
-                    SKD = SKD => ModelSegmentation
-                    sổ kinh doanh = SKD => ModelSegmentation
-                    trading book = SKD => ModelSegmentation
-                    TB = SKD => ModelSegmentation
-                    Gold = Gold => ModelSegmentation
-                    XAU (thuộc: commodity = Gold => ModelSegmentation
-                    giao dịch hàng hóa) = Gold => ModelSegmentation
+                    
                     TUNGLAN = Cho vay từng lần => ModelSegmentation
                     NONREVOL = Cho vay từng lần => ModelSegmentation
                     NONREVOLVING = Cho vay từng lần => ModelSegmentation
@@ -357,47 +340,7 @@ if st.button("Send"):
                     MobiFone = Mô hình sử dụng dữ liệu thay thế  => ModelSegmentation
                     Alternative data = Mô hình sử dụng dữ liệu thay thế  => ModelSegmentation
                     alternative = Mô hình sử dụng dữ liệu thay thế  => ModelSegmentation
-                    BL = Khách hàng Bán lẻ => ModelSegmentation
-                    Retail = Khách hàng Bán lẻ => ModelSegmentation
-                    TPCP = Định giá sản phẩm Giấy tờ có giá do Kho bạc Nhà nước phát hành => ModelName
-                    GOV BOND = Định giá sản phẩm Giấy tờ có giá do Kho bạc Nhà nước phát hành => ModelName
-                    TPCPBL/ CQDP = Định giá sản phẩm Giấy tờ có giá được Chính phủ bảo lãnh/Chính quyền địa phương phát hành => ModelName
-                    TPCPBL = Định giá sản phẩm Giấy tờ có giá được Chính phủ bảo lãnh/Chính quyền địa phương phát hành => ModelName
-                    CQDP = Định giá sản phẩm Giấy tờ có giá được Chính phủ bảo lãnh/Chính quyền địa phương phát hành => ModelName
-                    TPCPBL CQDP = Định giá sản phẩm Giấy tờ có giá được Chính phủ bảo lãnh/Chính quyền địa phương phát hành => ModelName
-                    Mô hình định giá Bond FI = Mô hình Định giá GTCG do TCTD phát hành => ModelName
-                    Mô hình định giá  FI Bond = Mô hình Định giá GTCG do TCTD phát hành => ModelName
-                    mô hình MtM = Mô hình định giá IRS VND => ModelName
-                    mô hình MTM = Mô hình định giá IRS VND => ModelName
-                    mô hình mark to market = Mô hình định giá IRS VND => ModelName
-                    mô hình USD SOFR ON = Mô hình định giá IRS USD tham chiếu SOFR ON => ModelName
-                    mô hình USD SOFRON = Mô hình định giá IRS USD tham chiếu SOFR ON => ModelName
-                    mô hình USD SOFR = Mô hình định giá IRS USD tham chiếu Term SOFR => ModelName
-                    mô hình USDR TERM SOFR = Mô hình định giá IRS USD tham chiếu Term SOFR => ModelName
-                    mô hình CCS SOFR ON = Mô hình định giá CCS USD/VND với chân USD thả nổi tham chiếu SOFR ON daily compounded in Arrears => ModelName
-                    mô hình CCS SOFR = Mô hình định giá CCS USD/VND với chân USD thả nổi tham chiếu Term SOFR => ModelName
-                    mô hình Var FX = Mô hình VaR cho danh mục kinh doanh ngoại tệ => ModelName
-                    mô hình Var FX SKD = Mô hình VaR cho danh mục kinh doanh ngoại tệ => ModelName
-                    mô hình Var FX tự doanh = Mô hình VaR cho danh mục kinh doanh ngoại tệ => ModelName
-                    mô hình VaR GTCG = Mô hình VaR lịch sử GTCG => ModelName
-                    mô hình VaR Bond = Mô hình VaR lịch sử GTCG => ModelName
-                    mô hình VaR PSLS = Mô hình VaR phái sinh lãi suất => ModelName
-                    mô hình VaR tổng = Mô hình VaR tổng Sổ kinh doanh => ModelName
-                    mô hình VaR.Total = Mô hình VaR tổng Sổ kinh doanh => ModelName
-                    mô hình Var total = Mô hình VaR tổng Sổ kinh doanh => ModelName
-                    MTM GOLD = Mô hình định giá danh mục vàng miếng tại VCB => ModelName
-                    Add-on FX JPY/VND = Mô hình định giá cho các giao dịch kinh doanh ngoại hối đối với cặp JPY/VND kỳ hạn trên 1 năm đến 2 năm => ModelName
-                    Add on FX JPY VND = Mô hình định giá cho các giao dịch kinh doanh ngoại hối đối với cặp JPY/VND kỳ hạn trên 1 năm đến 2 năm => ModelName
-                    Addon FX JPY/VND = Mô hình định giá cho các giao dịch kinh doanh ngoại hối đối với cặp JPY/VND kỳ hạn trên 1 năm đến 2 năm => ModelName
-                    Addon FX JPY-VND = Mô hình định giá cho các giao dịch kinh doanh ngoại hối đối với cặp JPY/VND kỳ hạn trên 1 năm đến 2 năm => ModelName
-                    MtM FI BOND CP 1Y = Mô hình định giá GTCG do TCTD phát hành có quyền chọn call put đồng thời kỳ hạn từ 1 năm trở xuống trên sổ kinh doanh	 => ModelName
-                    MtM FI BOND call put 1Y = Mô hình định giá GTCG do TCTD phát hành có quyền chọn call put đồng thời kỳ hạn từ 1 năm trở xuống trên sổ kinh doanh	 => ModelName
-                    MtM GTCG SNH = Mô hình định giá giao dịch GTCG TCTD SNH => ModelName
-                    MtM GTCG BB = Mô hình định giá giao dịch GTCG TCTD SNH => ModelName
-                    MtM GTCG ALM = Mô hình định giá giao dịch GTCG TCTD SNH => ModelName
-                    MtM CPCP USD SNH = Mô hình định giao dịch TPCP USD SNH => ModelName
-                    mô hình Var FI Bond = Mô hình VaR lịch sử GTCG do TCTD phát hành => ModelName
-                    mô hình Var Bond FI = Mô hình VaR lịch sử GTCG do TCTD phát hành => ModelName
+                    
                     Cho vay không tuần hoàn còn có thể giải ngân = Cho vay không tuần hoàn trong hiệu lực giải ngân và còn hạn mức chưa sử dụng => ModelSegmentation
                     NONR còn có thể giải ngân = Cho vay không tuần hoàn trong hiệu lực giải ngân và còn hạn mức chưa sử dụng => ModelSegmentation
                     NONREVOL còn có thể giải ngân = Cho vay không tuần hoàn trong hiệu lực giải ngân và còn hạn mức chưa sử dụng => ModelSegmentation
@@ -501,7 +444,7 @@ if st.button("Send"):
         # Tạo query và execute
         attempt = 0
         error_message = None
-        max_attempts = 3
+        max_attempts = 2
         info_dict["previous_error"] = ""
         flag_fail = 0
         flag_success = 0
